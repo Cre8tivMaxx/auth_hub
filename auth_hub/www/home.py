9a0a0363939f2de9d1ceb3a4b1e4edd72a2bc4db
notabdelrahmanelsayed <bedoelsayed785@gmail.com>
2026-01-18 11:41:31 +0200

    chore: home page blank template

---
+++ auth_hub/www/home.py
@@ -0,0 +1,6 @@
+def get_context(context):
+	context.sites = ["Capital", "demo", "majd", "Other Site", "ERP16 site"]
+	context.status = ["Enabled", "Disabled"]
+	context.installed_apps = ["ERPnext", "CRM", "HRMs", "Lms", "Education", "Payments"]
+	context.Description = "It will be always a short description like two lines maximum"
+	context.login = "https://demo-capital.cre8tiv-maxx.com/"
