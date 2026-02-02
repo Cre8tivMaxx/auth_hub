007a9cdc360e8fd998199bf8c06c03dc7b54c547
NotAbdelrahmanelsayed <bedoelsayed785@gmail.com>
2026-02-02 14:45:32 +0200

    feat(api): create user
    commit 9e2a34297dee60819c64b1510d40e942e39e0c52
    Author: NotAbdelrahmanelsayed <bedoelsayed785@gmail.com>
    Date:   Mon Feb 2 14:41:57 2026 +0200
    
        docs(api): change docs to google style
    
    commit 4d29a7479fee3b41c866251c8e62ec65dfb64894
    Author: NotAbdelrahmanelsayed <bedoelsayed785@gmail.com>
    Date:   Mon Feb 2 14:37:54 2026 +0200
    
        feat(api): create user

--- auth_hub/api/api.py
+++ auth_hub/api/api.py
@@ -1,39 +1,61 @@
 import frappe
 
+# TODO: Setup a logger that log errors, messages inside (logs/auth_hub.error.log, logs/auth_hub.log)
+
 
 @frappe.whitelist(allow_guest=True)
 def ping():
-	"""
-	Ping the site to make sure the APIs works.
-	Returns:
-		bool: "pong"
-	Endpoint:
-		GET <website>/api/method/auth_hub.api.ping
-	Example:
-		>>> ping()
-		"pong"
+    """Ping the site to make sure the APIs works.
 
-	Accessible through <website>/api/method/auth_hub.api.ping
-	"""
-	return "pong"
+    Example:
+        >>> ping()
+        "pong"
+
+    Returns:
+        str: "pong"
+
+    Note:
+        Accessible through <website>/api/method/auth_hub.api.ping
+    """
+    return "pong"
 
 
 @frappe.whitelist(allow_guest=True)
 def ensure_signed_up(email):
-	"""
-	    Verify if a user is already signed up in the system.
-	    Args:
-	            email (str): The email address to check for user existence.
-	    Returns:
-	            bool: True if a user with the given email exists, False otherwise.
-	    Endpoint:
-	            GET <website>/api/method/auth_hub.api.ensure_signed_up?email={email}
-	    Example:
-	            >>> ensure_signed_up("user@example.com")
-	            True
-	Accessible through <website>/api/method/auth_hub.api.ensure_signed_up?email={email}
-	"""
-	return bool(frappe.db.exists("User", email))
+    """Verify if a user is already signed up in the system.
+
+    Args:
+        email (str): The email address to check for user existence.
+
+    Returns:
+        bool: True if a user with the given email exists, False otherwise.
+
+    Example:
+        >>> ensure_signed_up("user@example.com")
+        True
+
+    Note:
+        Accessible through <website>/api/method/auth_hub.api.ensure_signed_up?email={email}
+    """
+    return bool(frappe.db.exists("User", email))
 
 
-# @frappe.whitelist(all)
+def create_user(user: dict):
+    """Create a new user on the system.
+
+    Args:
+        user (dict): dict of user's data, this should be injected from
+            `frappe.get_doc("User", user_email).as_dict()`.
+
+    Returns:
+        bool: True if the user was successfully created, False otherwise.
+
+    Note:
+        Accessible through <website>/api/method/auth_hub.api.create_user?user={user}
+    """
+    try:
+        frappe.get_doc(user).insert(ignore_permissions=True)
+    except Exception as e:
+        # TODO: Log the error.
+        return False
+    return True
