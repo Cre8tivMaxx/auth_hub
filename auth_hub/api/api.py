8a8269ae647009789eaa68875cc300f8b9ea3518
NotAbdelrahmanelsayed <bedoelsayed785@gmail.com>
2026-02-03 11:18:44 +0200

    feat(api): create permissions
    docs(permissions): write docs for methods in permissions.py
    chore: whitelist apis

--- auth_hub/api/api.py
+++ auth_hub/api/api.py
@@ -8,11 +8,11 @@ def ping():
     """Ping the site to make sure the APIs works.
 
     Example:
-        >>> ping()
-        "pong"
+    >>> ping()
+    "pong"
 
     Returns:
-        str: "pong"
+    str: "pong"
 
     Note:
         Accessible through <website>/api/method/auth_hub.api.ping
@@ -25,17 +25,17 @@ def ensure_signed_up(email):
     """Verify if a user is already signed up in the system.
 
     Args:
-        email (str): The email address to check for user existence.
+    email (str): The email address to check for user existence.
 
     Returns:
-        bool: True if a user with the given email exists, False otherwise.
+    bool: True if a user with the given email exists, False otherwise.
 
     Example:
-        >>> ensure_signed_up("user@example.com")
-        True
+    >>> ensure_signed_up("user@example.com")
+    True
 
     Note:
-        Accessible through <website>/api/method/auth_hub.api.ensure_signed_up?email={email}
+    Accessible through <website>/api/method/auth_hub.api.ensure_signed_up?email={email}
     """
     return bool(frappe.db.exists("User", email))
 
@@ -45,14 +45,13 @@ def create_user(user: dict):
     """Create a new user on the system.
 
     Args:
-        user (dict): dict of user's data, this should be injected from
-            `frappe.get_doc("User", user_email).as_dict()`.
-
+    user (dict): dict of user's data, this should be injected from
+        `frappe.get_doc("User", user_email).as_dict()`.
     Returns:
-        bool: True if the user was successfully created, False otherwise.
+    bool: True if the user was successfully created, False otherwise.
 
     Note:
-        Accessible through <website>/api/method/auth_hub.api.create_user?user={user}
+    Accessible through <website>/api/method/auth_hub.api.create_user?user={user}
     """
     try:
         frappe.get_doc(user).insert(ignore_permissions=True)
