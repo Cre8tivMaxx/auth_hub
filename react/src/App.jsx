6a0c3243094e1d38575d4cfd51762b729fc790cf
frappe <frappe@DESKTOP-MFLOG64.localdomain>
2026-02-05 23:03:35 +0200

    Home-Page-structure-done

---
+++ react/src/App.jsx
@@ -0,0 +1,19 @@
+// App.jsx
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./Components/Layout"
+import Home from "./pages/Home.jsx";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout/>}>
+          <Route path="/" element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
