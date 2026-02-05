41c491958282a1e2db95b6bbecad3151cb739179
frappe <frappe@DESKTOP-MFLOG64.localdomain>
2026-02-05 23:11:58 +0200

    updating home.py file

--- auth_hub/www/home.py
+++ auth_hub/www/home.py
@@ -1,6 +1,11 @@
-def get_context(context):
-	context.sites = ["Capital", "demo", "majd", "Other Site", "ERP16 site"]
-	context.status = ["Enabled", "Disabled"]
-	context.installed_apps = ["ERPnext", "CRM", "HRMs", "Lms", "Education", "Payments"]
-	context.Description = "It will be always a short description like two lines maximum"
-	context.login = "https://demo-capital.cre8tiv-maxx.com/"
+import frappe
+
+@frappe.whitelist(allow_guest=True)
+def get_context():
+    return {
+        "sites": ["Capital", "demo", "majd", "Other Site", "ERP16 site"],
+        "status": ["Enabled", "Disabled"],
+        "installed_apps": ["ERPnext", "CRM", "HRMs", "Lms", "Education", "Payments"],
+        "Description": "It will be always a short description like two lines maximum",
+        "login": "https://demo-capital.cre8tiv-maxx.com/"
+    }
