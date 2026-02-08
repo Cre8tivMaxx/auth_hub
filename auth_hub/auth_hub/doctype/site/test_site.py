26c9460fa7249c597f3fcd4c91785fabfc706996
NotAbdelrahmanelsayed <bedoelsayed785@gmail.com>
2026-02-08 10:48:00 +0200

    Revert "feat(Site): create site doctype"
    This reverts commit 482647bb2582f3e1a772a8c4e1d5c35d40567bb3.

--- auth_hub/auth_hub/doctype/site/test_site.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2026, Abdelrahman Elsayed and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestSite(FrappeTestCase):
-	pass
